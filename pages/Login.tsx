e9518f40faa201913f6788fa1aea43ccef2d39db
Bilegtgun HOP3 <bilegtgunhop3@iMac2021-02s-iMac.local>
2023-05-09 19:35:21 +0800

    login done

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,70 +1,93 @@
 import React, { useState } from "react";
 import { View, Text, Button, Pressable, SafeAreaView, StyleSheet, useWindowDimensions, TextInput } from "react-native";
 import Profile from "../assets/Profile";
-import DatePicker from "react-native-date-picker";
-import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
-import RNDateTimePicker from "@react-native-community/datetimepicker";
+import MaskInput, { Masks } from 'react-native-mask-input';
+import Header from "../components/Header";
+
+
 
 const Login = () => {
   const { width, height } = useWindowDimensions()
-  const [show, setShow] = useState(false)
-  const [mode, setMode] = useState('date')
-  const [text, setText] = useState('Empty')
-  const [date, setDate] = useState(new Date())
-
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date; 
-    setShow(!show); 
-    setDate(currentDate);
-    let tempDate = new Date(currentDate);
-    let fDate = tempDate.getDate() + '/' + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear(); 
-    setText(fDate)
+  const [bday, setBday] = useState('');
+  const [text, setText] = useState('');
+  const Continue = () => {
+    alert(bday + " " + text)
   }
-    const showMode = (currentMode) => {
-      setShow(true);
-      setMode(currentMode);
+  const styles = StyleSheet.create({
+    text: {
+      fontWeight: '500',
+      fontSize: 24,
+      textAlign: 'center'
+    },
+    text2: {
+      fontWeight: '700',
+      color: '#5CB881',
+      fontSize: width / 20,
+      paddingStart: 15
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: '#D9D9D9',
+      paddingStart: 15,
+      marginTop: 15,
+      fontWeight: '700',
+      width: width / 1.3,
+      height: width / 10,
+      borderRadius: width / 13,
+    },
+    button: {
+      width: width / 1.3,
+      height: width / 10,
+      backgroundColor: '#5CB881',
+      borderRadius: width / 13,
+      justifyContent: 'center',
+      marginTop: 15,
+    },
+    container: {
+      borderColor: '#D9D9D9',
+      borderWidth: 1,
+      width: width * 0.9,
+      height: height / 2,
+      borderRadius: width / 12,
+      flexDirection: 'column',
+      justifyContent: 'space-evenly',
+      alignItems: 'center'
     }
-      return (
-        <SafeAreaView style={{ width: width, height: height, alignItems: 'center', justifyContent: 'center', gap: 30 }}>
-          <Text style={styles.text}>Сайн уу?</Text>
-          <View style={{ borderColor: '#D9D9D9', borderWidth: 1, width: width * 0.9, height: height / 2, borderRadius: width / 12, alignItems: 'center' }}>
-            <Profile width={width / 3} />
-            <Text>Нэр</Text>
-            <Text style={styles.text}>Birth Date :</Text>
-            {/* <DatePicker
-          style={styles.datePickerStyle}
-          date={date}
-          mode="date"
-          confirmText="Confirm"
-          cancelText="Cancel"
-          onDateChange={(date) => {
-            setDate(date);
-          }}
-        /> */}
-            {show && (
-              <RNDateTimePicker
-                value={date}
-                display="calendar"
-                style={{ backgroundColor: 'white', borderColor: 'gray', borderWidth: 1, }}
-                onChange={onChange}
-              />)}
-              <Text>{text}</Text>
-              <Button onPress={showMode} title="dasdasd"/>
-          </View>
-          <TextInput placeholder={text} style={{borderWidth: 1, width:100, height:20}}/>
-        </SafeAreaView>
-      );
-    }
+  });
+  return (
+    <SafeAreaView style={{ width: width, height: height, alignItems: 'center', gap: 30 }}>
+      <Text style={styles.text}>Сайн уу?</Text>
+      <View style={styles.container}>
+        <Profile width={width / 3} />
+        <View>
+          <Text style={styles.text2}>Нэр</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Нэр"
+            onChangeText={newText => setText(newText)} />
+        </View>
+        <View>
+          <Text style={styles.text2}>Төрсөн өдөр</Text>
+          <MaskInput
+            style={styles.input}
+            placeholder="Он/сар/өдөр"
+            value={bday}
+            onChangeText={(masked, unmasked) => {
+              setBday(masked);
+            }}
+            mask={Masks.DATE_YYYYMMDD}
+          />
+        </View>
 
-    export default Login;
+        <Pressable
+          onPress={Continue}
+          style={styles.button}>
+          <Text style={{ textAlign: 'center', color: 'white', fontWeight: '700' }}>Цааш үргэлжлүүлэх</Text>
+        </Pressable>
+      </View>
 
-    export const styles = StyleSheet.create({
-      text: {
-        fontWeight: '500',
-        fontSize: 24,
-        textAlign: 'center'
-      },
-      datePickerStyle: {
-        width: 230,
-      },
-    });
+    </SafeAreaView>
+  );
+}
+
+export default Login;
