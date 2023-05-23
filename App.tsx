adbd01eff0f773932a85175ccaad2f830a855cfa
n0wsh <galbadrakhtergel0820@gmail.com>
2023-05-24 02:25:51 +0800

    save

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import ExpensePage from "./pages/Expense";
 import ExpenseMonthPage from "./pages/ExpenseMonth";
 import HomeScreen from "./pages/Home";
 import Settings from "./pages/Settings";
+import HomeScreen from "./pages/Home";
 
 const Stack = createNativeStackNavigator();
 
