588afec5f916e61b17185dd458a3740547cb1a80
n0wsh <galbadrakhtergel0820@gmail.com>
2023-05-24 02:25:51 +0800

    save

--- App.tsx
+++ App.tsx
@@ -12,11 +12,8 @@ import Setting from "./assets/Setting";
 import Bottombar from "./components/Footer";
 import ExpensePage from "./pages/Expense";
 import ExpenseMonthPage from "./pages/ExpenseMonth";
-<<<<<<< HEAD
-import HomeScreen from "./pages/Home";
-=======
->>>>>>> c06f599 (settings done)
 import Settings from "./pages/Settings";
+import HomeScreen from "./pages/Home";
 
 const Stack = createNativeStackNavigator();
 
